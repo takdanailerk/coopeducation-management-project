5f7bfd553c924fe18ec75108ce0b44d04f9079ae
Takdanai Lerksakunchai <takdanaireaksakunchai@gmail.com>
2024-07-18 16:07:34 +0700

    remove unused files

--- src/main/java/itsci/mju/coopeducation_management_project/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package itsci.mju.coopeducation_management_project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import itsci.mju.coopeducation_management_project.model.User;
-
-public interface UserRepository extends JpaRepository<User, String> {
-
-}
